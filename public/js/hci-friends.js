5f6bca7bb9479d2a53b0669a0090613e3ebe7149
karinicolegarcia <50600191+karinicolegarcia@users.noreply.github.com>
2021-02-04 14:27:26 -0800

    Made a bunch of changes

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,7 +3,14 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$('a.friend-name').click(changeText);
 })
+
+function changeText(event) {
+	event.preventDefault();
+	var name = $(this).text();
+	$(this).text(anagrammedName(name));
+}
 
 /*
  * Function that is called when the document is ready.
@@ -14,10 +21,10 @@ function initializePage() {
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
-	} 
+	}
 	else if (name == "Ivan Sutherland") {
 		return "Vandal Heist Run";
 	}
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
+exports.addFriend = function(request, response) {
 	// Your code goes here
- }
+  response.render('index.handlebars', data);
+
+  var newFriend =
+  	{
+  		"name": request.query.name,
+  		"description": request.query.description,
+  		"imageURL": "https://i.picsum.photos/id/1025/500/500.jpg?hmac=-8oa3YhiI2vz-AJSkoxWl_7uP0QpVYMmQabi48iqMHM"
+  	};
+
+  data.friends.push(newFriend);
+
+}
